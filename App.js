8a18aa17966d05a03fef499391c335ca675026ab
Leevi Ketola <leevi.ketola@tuni.fi>
2023-02-06 18:18:05 +0200

    create native android project

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StrictMode } from 'react-native';
+
 
 export default function App() {
   return (
+    <StrictMode>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </StrictMode>
   );
 }
 
