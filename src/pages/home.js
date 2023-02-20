08f6cf0c88027576716d5155a57022a9b414050a
Leevi Ketola <leevi.ketola@tuni.fi>
2023-02-20 19:36:16 +0200

    implement first time home screen view

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
-import { StyleSheet, View, StatusBar } from 'react-native';
-import { Text, Divider } from "react-native-paper";
+import { StyleSheet, View, StatusBar, TouchableOpacity } from 'react-native';
+import { Text, IconButton, Avatar } from "react-native-paper";
 import { Provider as StoreProvider } from 'react-redux';
+import { LinearGradient } from 'expo-linear-gradient';
 import store from '../app/store';
 
 export default function Home( { navigation } ) {
@@ -11,10 +12,60 @@ export default function Home( { navigation } ) {
                 <View style={styles.rectangle}>
                     <Text style={styles.welcomeText}>Etusivu</Text>
                     <View style={styles.divider} />
+                    <Text style={styles.infoText}>
+                        Aloita luomalla itsellesi budjetti.{'\n'} 
+                        Voit halutessasi luoda enemmän kuin yhden budjetin.
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => console.log('Pressed')}
+                        style={styles.button}>
+                            <LinearGradient
+                                colors={['rgba(23, 181, 173, 0.81)', 'rgba(77, 182, 241, 0.85)']}
+                                style={styles.gradient}
+                            />
+                            
+                            <Avatar.Icon size={50} color='#000000' icon="pulse" style={styles.buttonIcon}/>
+                            <Text style={styles.buttonText}>Siirry budjetoimaan</Text>
+                            <Avatar.Icon size={50} color='#000000' icon="play" style={styles.buttonIcon}/>
+                    </TouchableOpacity>
+
                 </View>
+                
                 <View style={styles.header}>
                     <Text style={styles.headerText}>Eva - OmaBudjetti</Text>
                 </View>
+            </View>
+            <View style={styles.bottomNavbar}>
+                <IconButton
+                        icon="home"
+                        iconColor={'#fff'}
+                        size={30}
+                        onPress={() => console.log('Pressed Home')}
+                />
+                <IconButton
+                        icon="image-frame"
+                        iconColor={'#fff'}
+                        size={30}
+                        onPress={() => console.log('Pressed Home')}
+                />
+                <IconButton
+                        icon="image-frame"
+                        iconColor={'#fff'}
+                        size={30}
+                        onPress={() => console.log('Pressed Home')}
+                />
+                <IconButton
+                        icon="image-frame"
+                        iconColor={'#fff'}
+                        size={30}
+                        onPress={() => console.log('Pressed Home')}
+                />
+                <IconButton
+                        icon="format-list-bulleted"
+                        iconColor={'#fff'}
+                        size={30}
+                        onPress={() => console.log('Pressed Home')}
+                />
             </View>
         </StoreProvider>
     );
@@ -28,6 +79,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-start',
         flexDirection: 'column',
+        overflow: 'hidden',
         marginTop: StatusBar.currentHeight || 0,
     },
     header: {
@@ -51,9 +103,59 @@ const styles = StyleSheet.create({
     divider: {
         marginTop: 4,
         marginLeft: 25,
-        marginRight: 30,
+        marginRight: 25,
         borderColor: 'black',
         borderBottomWidth: 2,
+    },
+    infoText: {
+        textAlign: 'center',
+        marginTop: 60,
+        marginLeft: 30,
+        marginRight: 30,
+        color: '#000000',
+        fontFamily: 'Roboto',
+        fontSize: 22,
+    },
+    button: {
+        marginTop: 60,
+        marginLeft: 30,
+        marginRight: 30,
+        borderWidth: 0,
+        borderRadius: 60,
+        borderColor: 'rgba(255,255,255,0)',
+        backgroundColor: '#F8F9F9', 
+        flexDirection: 'row',
+        alignItems: 'center', 
+        justifyContent: 'space-between',
+        overFlow: 'hidden',
+        
+    },
+    gradient: {
+        position: 'absolute',
+        left: -2,
+        right: -2,
+        top: -2,
+        bottom: -2,
+        borderWidth: 0,
+        borderRadius: 60,
+    },
+    buttonIcon: {
+        backgroundColor: 'rgba(255,255,255,0)',
+        color: '#000000',
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        fontSize: 22,
+    },
+    bottomNavbar: {
+        positon: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        backgroundColor: '#17B5AD',
     },
     rectangle: {
         position: 'absolute',
@@ -63,7 +165,6 @@ const styles = StyleSheet.create({
         height: 800,
         backgroundColor: '#fff',
         borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        
+        borderTopRightRadius: 20,        
     },
 });
