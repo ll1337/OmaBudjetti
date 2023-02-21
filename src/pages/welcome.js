8051469cb39585c4f0f42f1062f7de7479ce4e1a
Daniil Lukin <daniil.lukin@tuni.fi>
2023-02-21 16:52:59 +0200

    adjust alignments, integrate PrettyButton

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Linking} from 'react-native';
 import { Text, Checkbox, Button } from "react-native-paper";
 import { Provider  as StoreProvider } from 'react-redux';
 import store from '../app/store';
+import PrettyButton from '../components/prettyButton';
 
 export default function Welcome( { navigation } ) {
     const [checked, setChecked] = React.useState(false);
@@ -10,24 +11,18 @@ export default function Welcome( { navigation } ) {
     return(
         <StoreProvider store={store}>
             <View style={styles.container}>
-                <View style={styles.welcomeView}>
-                    <Text style={styles.welcomeText}>Tervetuloa</Text>
-                </View>
 
-                <View style={styles.headerView}>
+                    <Text numberOfLines={1} style={styles.welcomeText}>Tervetuloa</Text>
                     <Text numberOfLines={1} style={styles.headerText}>Eva - OmaBudjettiin</Text>
-                </View>
-
-                <View style={styles.textView}>
 
                     <Text style={styles.textText}>
                     • Tämän sovelluksen tarkoituksena on auttaa sinua saamaan taloutesi hallintaansi
-                    luomalla oman elämäsi todellinen budjetti {"\n"}
+                    luomalla oman elämäsi todellinen budjetti
                     </Text>
 
                     <Text style={styles.textText}>
                     • Ensimmäisellä kerralla, kun asetat budjettisi, sovellus ohjaa sinua vaihe vaiheelta
-                    eteenpäin {"\n"}
+                    eteenpäin 
                     </Text>
 
                     <Text style={styles.textText}>
@@ -35,37 +30,30 @@ export default function Welcome( { navigation } ) {
                     muokata olemassa olevaa budjettia helposti
                     </Text>
 
-                </View>
+                    <View style={styles.checkbox}>
+                        <Checkbox 
+                            color='#17B5AD'
+                            uncheckedColor='#17B5AD'
+                            status={checked ? 'checked' : 'unchecked'}
+                            onPress={() => {
+                                setChecked(!checked);
+                            }}
+                        />
 
-                <View style={styles.checkbox}>
-                    <Checkbox 
-                        color='#17B5AD'
-                        uncheckedColor='#17B5AD'
-                        status={checked ? 'checked' : 'unchecked'}
-                        onPress={() => {
-                            setChecked(!checked);
-                        }}
-                    />
-
-                </View>
-                <View style={styles.linkView}>
-                    <Text style={styles.linkText}
-                        onPress={() => Linking.openURL('http://google.com')}>
-                        Olen lukenut ja hyväksynyt käyttöehdot
-                    </Text>
-                </View>
-
-                <View style={styles.button}>
-                    <Button
+                        <Text numberOfLines={1} style={styles.linkText}
+                            onPress={() => console.log('Link pressed')}>
+                            Olen lukenut ja hyväksynyt käyttöehdot
+                        </Text>
+                    </View>
+                    
+                    <View style={styles.button}>
+                    <PrettyButton
+                        onPress={() => console.log('Button pressed')} 
+                        title="Eteenpäin"
                         disabled={!checked}
-                        mode="outlined"
-                        buttonColor='#F8F9F9'
-                        textColor='#000000'
-                        onPress={() => Linking.openURL('http://google.com')}
-                        >
-                    Eteenpäin
-                    </Button>
-                </View>
+                        />
+
+                    </View>
             </View>
         </StoreProvider>
     )
@@ -77,85 +65,57 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#F8F9F9"
     },
-        welcomeView: {
-        flex: 1, 
-        position: 'absolute',
-        alignItems: 'center',
-        width: 287,
-        height: 65,
-        left: '10%',
-        top: '10%'
-    },
     welcomeText: {
-        flex: 1, 
-        fontSize: 55,
+        textAlign: 'center',
+        marginLeft: '5%',
+        marginRight: '5%',
+        marginTop: '25%',
+        fontSize: 60,
         fontFamily: 'Roboto',
         color: '#17B5AD'
-    },
-    headerView: {
-        flex: 1, 
-        position: 'absolute',
-        alignItems: 'center',
-        width: 299,
-        height: 45,
-        left: '8%',
-        top: '19%'
     },
     headerText: {
-        flex: 1, 
-        alignItems: 'center', 
-        fontSize: 32,
+        textAlign: 'center', 
+        marginLeft: '5%',
+        marginRight: '5%',
+        marginTop: '-4%',
+        fontSize: 34,
         fontFamily: 'Roboto',
         color: '#17B5AD'
     },
-    textView: {
-        flex: 1, 
-        position: 'absolute',
-        alignItems: 'center',
-        width: 300,
-        height: 255,
-        left: '8%',
-        top: '28%'
-    },
     textText: {
+        textAlign: 'center',
+        marginLeft: '5%',
+        marginRight: '5%',
         fontSize: 16,
+        marginTop: '10%',
         fontFamily: 'Roboto',
         fontWeight: '400',
         color: "#000000",
         lineHeight: 20
     },
     checkbox: {
-        flex: 1, 
+        flex: 1,
+        alignContent: 'center',
         position: 'absolute',
-        alignItems: 'center',
-        width: 21,
-        height: 21,
-        left: 41,
-        top: '70%',
-    },
-    linkView: {
-        flex: 1, 
-        position: 'absolute',
-        alignItems: 'center',
-        width: 274,
-        height: 20,
-        left: 62,
-        top: '71%'
+        marginTop: '150%',
+        marginLeft: '6%',
+
     },
     linkText: {
         color: '#17B5AD',
         textDecorationLine: 'underline',
         lineHeight: 20,
-        fontWeight: "700"
+        fontWeight: "700",
+        marginLeft: '11%',
+        marginRight: '9%',
+        marginTop: '-9%'
     },
     button: {
-        flex: 1, 
-        position: 'absolute',
-        alignItems: 'center',
-        width: 124,
-        height: 62,
-        left: 114,
-        top: '78%'
+        alignSelf: 'center',
+        marginTop: '30%',
+        marginLeft: '5%',
+        marginRight:'5%',
     }
 
 })
