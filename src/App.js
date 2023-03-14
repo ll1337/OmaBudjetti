fbcae9ecc468cb921b1f552b0be9b9b3266728a7
Daniil Lukin <daniil.lukin@tuni.fi>
2023-03-14 11:44:38 +0200

    implement transitions between landing, welcome and home

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         <PaperProvider>
           <NavigationContainer>
             <Stack.Navigator
-              initialRouteName="Home"
+              initialRouteName="Welcome"
               screenOptions={{
                 headerShown: false
               }}
